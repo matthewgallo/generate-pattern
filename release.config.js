dc0c8164da258bce519dd4844c5a9dddd103d9e0
Matt Gallo <mdgallo@us.ibm.com>
2024-11-12 22:30:25 -0500

    chore: update publish config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,20 @@
 export const config = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      'preset': 'conventionalcommits',
+      'releaseRules': [
+          { 'type': 'docs', 'scope': 'README', 'release': 'minor' },
+          { 'type': 'refactor', 'release': 'minor' },
+          { 'type': 'style', 'release': 'minor' },
+          { 'type': 'chore', 'release': 'minor' },
+          { 'type': 'fix', 'release': 'minor' },
+          { 'type': 'feat', 'release': 'minor' },
+        ],
+        'parserOpts': {
+          'noteKeywords': ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+    }],
     '@semantic-release/release-notes-generator',
     ["@semantic-release/git", {
       "assets": ["dist/*.js", "dist/*.js.map"],
