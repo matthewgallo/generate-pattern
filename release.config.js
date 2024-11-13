886a3628abae11050498f40b4c7ff4cc7bcacf5b
Matt Gallo <mdgallo@us.ibm.com>
2024-11-13 11:39:37 -0500

    chore: update release config, add missing packages

--- release.config.js
+++ release.config.js
@@ -16,7 +16,18 @@ export const config = {
         }
     }],
     '@semantic-release/release-notes-generator',
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
     ["@semantic-release/git", {
+      "assets": [
+        "CHANGELOG.md",
+        "dist/**",
+        "package.json",
+      ],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     '@semantic-release/github'
